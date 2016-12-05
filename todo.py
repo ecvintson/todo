3b0105476e23a805ab762b36693c64f73d9449be
ecvintson <ecvintson@gmail.com>
2016-12-05 15:32:15 -0500

    comments

--- todo.py
+++ todo.py
@@ -1,5 +1,7 @@
 import pickle
 
+# Will be a program for writing to-do lists. Will save lists in a pickle file, and 
+# pull them back out
 
 def saveList():
 	with open('save.pickle', 'wb') as handle:
