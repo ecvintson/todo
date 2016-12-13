3bbc4182995dc12575ba9d254c6e9fd45842585c
ecvintson <ecvintson@gmail.com>
2016-12-12 22:34:26 -0500

    v 0.2
    Added list
    Added basic functions
    Main loop now functioning

--- todo.py
+++ todo.py
@@ -3,6 +3,8 @@ import pickle
 # Will be a program for writing to-do lists. Will save lists in a pickle file, and 
 # pull them back out
 
+
+#pickle save/load functions
 def saveList():
 	with open('save.pickle', 'wb') as handle:
 		pickle.dump(todoList, handle)
@@ -12,16 +14,42 @@ def loadList():
 		listLoad = pickle.load(handle)
 		return listLoad
 
+#list to hold To Do items
+theList = ["first", "second"]
+
+# Functions that are called by user inputs
 def helloWorld():
 	print "Hello World!!"
 
+def quitCommand():
+	quit()	
+
+def addList():
+	x = raw_input()
+	theList.append(x)
+
+def printList():
+	print theList
+
+def remList():
+
+
+#library of commands, calling functions
 commands = {
-	'hello': helloWorld
+	'hello': helloWorld,
+	'list': printList,
+	'add': addList,
+	'remove': remList,
+	'save': 4,
+	'quit': quitCommand
 }
 
 
-print "To Do List 0.1"
 
-uComm= raw_input()
 
-commands[uComm]()
+print "To Do List 0.2"
+
+
+while True:
+	uComm= raw_input()
+	commands[uComm]()
